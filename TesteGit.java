8634ec577e8726b5b7cf7f4f0fde214990e3feac
DiCR <dicr@gmail.com>
2016-08-23 19:37:20 +0200

    Imprimindo a nova tarefa realizada

--- TesteGit.java
+++ TesteGit.java
@@ -3,6 +3,8 @@ public class TesteGit {
 	public static void main(String[] args) {
 		
 		System.out.println("Exemplo de teste de uso do GIT");
+
+		System.out.println("Fazendo o clone do Repositorio PM-87 Anotacoes");
 	
 	}
 
